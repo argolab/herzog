4f7cfcccdd374c751fb20663bd2b0576435f79f3
Mo Norman <ltaoist6@gmail.com>
2014-05-14 23:23:39 +0800

    add argo.py sdk

---
+++ argo.py
@@ -0,0 +1,64 @@
+from json import loads as json_parse
+from urllib import urlencode, urlopen
+
+ARGO_PREFIX = 'http://localhost:1996/bbsapi/'
+
+def argo_http(api, param=None, session=None):
+
+    '''Make a http post request of `ARGO_PREFIX`
+
+    session need dict() and .update function to
+     keep session data.'''
+
+    # merge args
+    if session :
+        args = dict(session)
+    else :
+        args = {}
+    if param :
+        args.update(param)
+
+    # parase data
+    text = urlopen(
+        ARGO_PREFIX + api, urlencode(args)
+    ).read().decode('utf-8')
+    if text[-1] == '!' :
+        seq = text.find('\n')
+        session.update(json_parse(text[seq+1:-1]))
+        return json_parse(text[0:seq])
+    return json_parse(text)
+
+class ArgoRPCClicent :
+
+    def __init__(self, session=None):
+        if session is None:
+            session = {}
+        self._session = session
+
+all_api = [
+    'test',
+    'do_test',
+    'do_login', 
+    'do_logout',
+    'utmpnum',
+    'queryuser',
+    'listmails',
+    'showmail',
+    'do_sendmail',
+    'do_delmail', 
+]
+
+for _api in all_api :
+    # Use a closure to keep value not ref
+    def closure():
+        api = _api
+        def inner(self, **param):
+            return argo_http(api, param, session=self._session)
+        inner.func_name = api
+        ArgoRPCClicent.__dict__[api] = inner
+    closure()
+
+if __name__ == "__main__" :
+    cc = ArgoRPCClicent()
+    # print cc.test()
+    print cc.queryuser(userid='sysop')
