a52717072e27015668d6b6823dbfd79d5adc3948
Mo Norman <ltaoist6@gmail.com>
2014-07-19 08:00:03 +0800

    add database scheme

---
+++ DATABASE.sql
@@ -0,0 +1,87 @@
+DROP TABLE IF EXISTS `argo_topic`;
+CREATE TABLE IF NOT EXISTS `argo_topic` (
+
+       `tid` int(11) unsigned NOT NULL auto_increment,
+       `boardname` varchar(40) NOT NULL,
+       `owner` varchar(40) NOT NULL,
+       -- `realowner` varchar(40) NOT NULL, -- 匿名版
+       `title` varchar(60) NOT NULL,
+       `posttime` timestamp NOT NULL ,
+       `score` int(40) NOT NULL,         -- 排名
+       `lastreply` timestamp NOT NULL,
+       `lastcomment` timestamp NOT NULL,
+       `lastupdate` timestamp NOT NULL,
+
+       `upvote` int(11) NOT NULL default 0,
+       `fromaddr` varchar(20) NOT NULL default '',
+       `fromapp` varchar(40) NOT NULL default '',   -- 来自哪个app
+
+       `summary` varchar(200) NOT NULL default '',  -- 摘要
+       `flag` int(20) NOT NULL default 0,           -- 属性
+
+       `oldfilename` varchar(32) NOT NULL default,  -- 对应的文件存储文件名
+
+       `content` mediumtext,
+
+       PRIMARY KEY tid,            -- For :  ORDER BY tid
+       KEY (boardname, tid)        -- For :  WHERE boardname= AND tid=
+       KEY (boardname, score)      -- For :  WHERE boardname= ORDER BY score
+       KEY score,                  -- For :  ORDER BY score
+       KEY owner,                  -- For :  WHERE owner=
+       KEY (boardname, owner)      -- For :  WHERE boardname= AND owner=
+       KEY (tid, owner)            -- For :  WHERE tid= AND owner=
+
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8 ;
+
+DROP TABLE IF EXISTS `argo_reply`;
+CREATE TABLE IF NOT EXISTS `argo_reply` (
+
+       `rid` int(11) unsigned NOT NULL auto_increment,
+
+       `tid` int(11) unsigned NOT NULL,
+       -- "WHERE tid= " is all post to topic[tid]
+       
+       `brid` int(11) unsigned NOT NULL,
+       -- "WHERE brid=rid" is all post reply to lz, else it is comment
+       -- "WHERE tid= ORDER BY brid" load all reply and comment by time
+
+       `replyid` int(11) unsigned NOT NULL,
+       -- "WHERE replyid=tid" is all post reply to lz
+
+       `owner` varchar(40) NOT NULL,
+       `title` varchar(60) NOT NULL,
+       `posttime` timestamp NOT NULL ,
+
+       `lastupdate` timestamp NOT NULL,
+
+       `fromaddr` varchar(20) NOT NULL default '',
+       `fromapp` varchar(40) NOT NULL default '',   -- 来自哪个app
+
+       `flag` int(20) NOT NULL default 0,           -- 属性
+       `oldfilename` varchar(32) NOT NULL default,  -- 对应的文件存储文件名
+
+       `content` mediumtext,
+
+       PRIMARY KEY rid               -- FOR : ORDER BY tid
+       KEY tid,                      -- FOR : WHERE tid=
+       KEY brid,                     -- FOR : WHERE brid=
+       KEY (tid, brid)               -- FOR : WHERE tid= AND brid=
+       KEY owner                     -- FOR : ORDER BY owner
+
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8 ;
+
+DROP TABLE IF EXISTS `argo_user_topic` (
+     `utid` int(11) unsigned NOT NULL auto_increment,
+
+     `userid` varchar(20) NOT NULL,
+     `tid` int(11) unsigned NOT NULL,
+
+     `flag` int(11) unsigned NOT NULL
+     `touchtime` timestamp NOT NULL default CURRENT_TIMESTAMP
+                  on update CURRENT_TIMESTAMP,
+
+      PRIMARY KEY utid,
+      KEY userid,
+      KEY tid,
+      KEY (userid, tid)
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8 ;
